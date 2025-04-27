41602971f8eafcb10dc817380144e795055d1f12
enesilbay <enesilbayy@gmail.com>
2025-04-28 00:42:28 +0300

    sitenin görünümünü değiştirir.

--- script.js
+++ script.js
@@ -1,19 +1,18 @@
 function filmAra() {
-    const arama = document.getElementById("aramaKutusu").value.toLowerCase();
-    const filmler = document.getElementsByClassName("film");
-  
-    for (let i = 0; i < filmler.length; i++) {
-      const baslik = filmler[i].getElementsByTagName("h3")[0].textContent.toLowerCase();
-      if (baslik.includes(arama)) {
-        filmler[i].style.display = "block";
-      } else {
-        filmler[i].style.display = "none";
-      }
+  const arama = document.getElementById("aramaKutusu").value.toLowerCase();
+  const filmler = document.getElementsByClassName("film");
+
+  for (let i = 0; i < filmler.length; i++) {
+    const baslik = filmler[i].getElementsByTagName("h3")[0].textContent.toLowerCase();
+    if (baslik.includes(arama)) {
+      filmler[i].style.display = "block";
+    } else {
+      filmler[i].style.display = "none";
     }
   }
-  
-  // Sayfa yüklendikten sonra input'a dinleyici ekleyelim:
-  window.addEventListener("DOMContentLoaded", function() {
-    document.getElementById("aramaKutusu").addEventListener("keyup", filmAra);
-  });
-  
+}
+
+// Sayfa yüklendikten sonra input'a dinleyici ekleyelim:
+window.addEventListener("DOMContentLoaded", function() {
+  document.getElementById("aramaKutusu").addEventListener("keyup", filmAra);
+});
